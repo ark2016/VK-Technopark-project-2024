2826fe59471b5fd23b3bf393e11656565a3316b6
MakarHome <masemennikov@gmail.com>
2024-12-13 16:41:15 +0300

    Update README.md and comments

--- metric_example.py
+++ metric_example.py
@@ -1,9 +1,9 @@
-# Только эти две функции, всё остальное - служебные
+# Импортируем только эти две функции из модуля, всё остальные в нём - служебные
 from metric_module.metric import coverage_percent, calculate_coverage_for_df
 
 import pandas as pd
 
-
+# В начале кода не должно быть переноса строки, код должен начинаться с def
 code = '''def is_even(n):
     if n < 0:
         print("Negative number, can't be checked for evenness.")
@@ -15,6 +15,7 @@ code = '''def is_even(n):
             print("One is odd!")
         return False'''
 
+# То же самое для тестирующей функции
 test = '''def test_is_even():
     assert is_even(4) is True
     assert is_even(0) is True
@@ -62,10 +63,11 @@ test3 = '''def test_remove_punctuation_from_string():
     assert remove_punctuation_from_string("no_punctuation") == "nopunctuation"
 '''
 
-
 if __name__ == '__main__':  # Это здесь обязательно, если на Windows, так как используется мультипроцессинг
 
-    # Рассчитаем метрики для отдельных строк
+    # /Рассчитаем метрики для отдельных строк/ #
+
+    # Процент покрытия, количество ошибок выполнения, список пропущенных строк
     percent1, errors1, missing1 = coverage_percent(code, test)
     percent2, errors2, missing2 = coverage_percent(code2, test2)
     percent3, errors3, missing3 = coverage_percent(code3, test3)
@@ -74,10 +76,13 @@ if __name__ == '__main__':  # Это здесь обязательно, если
     print(f'Test 2. Процент покрытия: {percent2} %, Количество ошибок: {errors2}, Пропущенные строчки: {missing2}')
     print(f'Test 2. Процент покрытия: {percent3} %, Количество ошибок: {errors3}, Пропущенные строчки: {missing3}')
 
-    # Рассчитаем метрики для целого датафрейма
+    # /Рассчитаем метрики для целого датафрейма/ #
     df = pd.read_csv("data/04_functions_tests_1200.csv")
 
     # Рассчитываем покрытие и ошибки для каждой строки в DataFrame
+    # ВАЖНО! Функция никуда не записывает result_df, она только его возвращает
+
+    # Итоговый df с метриками, среднее значение покрытия, среднее значение ошибок выполнения
     result_df, mean_cov, mean_err = calculate_coverage_for_df(df)
     result_df.to_csv("tests_metric.csv", index=False)
 
